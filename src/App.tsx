8d2b75b46ce9f280c6abc5ecda1df31f87dc2933
Esrael Dawit <79444726+EsraelDawit-a@users.noreply.github.com>
2022-11-02 16:08:08 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App bg-red-400">
+      hellow
+    </div>
+  )
+}
+
+export default App
